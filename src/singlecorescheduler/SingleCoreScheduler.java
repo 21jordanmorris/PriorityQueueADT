9ca1dfb4f215879dafe236da6224de5d26d58025
Jordan Morris <21jordanrmorris@gmail.com>
2018-09-23 23:58:31 -0500

    Finished Random Mode, project fully functions

--- src/singlecorescheduler/SingleCoreScheduler.java
+++ src/singlecorescheduler/SingleCoreScheduler.java
@@ -37,50 +37,80 @@ public class SingleCoreScheduler
 
         PrintStream outFile = new PrintStream(new File("simulatedjobsoutput.txt"));
 
-        //RANDOM MODE
+        // Values used to find averages
+        double totalWait = 0;
+        double numberComp = 0;
+
+        // Values for PCB - Initialization
+        int id = 0, priority = 0, cycleStart = 0, length = 0;
+
+        // Random Mode
         if(mode == 'r' || mode == 'R') {
+
+            // P Value - Command Line Input
             double randProb = Double.parseDouble(args[2]);
-            if(randProb < .01 || randProb > 1.00)
+            if (randProb < .01 || randProb > 1.00)
                 System.out.println("Parameter args[2] is not a valid input.");
             else {
-                double rNumber;
-                for(int i = 1; i <= numberCycles; i++) {
+
+                int idCounter = 1;
+
+                // Random Priority
+                Random rPriorityLength = new Random(System.currentTimeMillis());
+
+                // Random Length
+
+                for (int i = 0; i <= numberCycles; i++) {
+
+                    // Q Value - Random Probability Value
+                    Random rand = new Random();
+                    double rNumber = rand.nextDouble();
+
                     //Cycle line 1 - cycle count
-                    outFile.println("*** Cycle #: " + i);
+                    outFile.println("*** Cycle #: " + i + " ***");
 
                     //Cycle line 2 - CPU current task
-                    if(readyQ.isEmpty())
+                    if (readyQ.isEmpty()) {
                         outFile.println("The CPU is idle.");
-                    else
-                    {
+                    } else {
+                        if (!(readyQ.peek().isExecuting())) {
+                            readyQ.peek().execute();
+                            readyQ.peek().setStart(i);
+                            readyQ.peek().setWait(readyQ.peek().getStart() - readyQ.peek().getArrival());
+                            totalWait += readyQ.peek().getStart() - readyQ.peek().getArrival();
+                        }
+                        if (readyQ.peek().getLength() == (i - readyQ.peek().getStart())) {
+                            int pid = readyQ.peek().getPid();
+                            readyQ.remove();
+                            outFile.printf("Process #%d has just been terminated.%n", pid);
+                            numberComp++;
+                        } else
+                            outFile.printf("Process #%d is executing.%n", readyQ.peek().getPid());
                     }
 
                     //Cycle line 3 - Add new job if applicable
-                    if(false) {
-
-                    }
-                    else
-                    {
+                    if (rNumber <= randProb) {
+                        id = idCounter;
+                        idCounter++;
+                        priority = rPriorityLength.nextInt(39) - 19;
+                        length = rPriorityLength.nextInt(99) + 1;
+                        PCB newPCB = new PCB(id, priority, 0, i, length);
+                        readyQ.insert(newPCB);
+                        outFile.printf("Adding job with pid #%d and priority %d and length %d.%n",
+                                id, priority, length);
+                    } else
                         outFile.println("No new job this cycle.");
-                    }
                 }
             }
         }
 
-
-
-
-
-        //FILE MODE
         else if(mode == 'f' || mode == 'F') {
             Scanner inFile = new Scanner(new FileReader(args[2]));
 
-            //INITIALIZATION
-            int id = 0, priority = 0, cycleStart = 0, length = 0;
+            // Initialization
             boolean iCS = true;
 
-            double numberComp = 0, totalWait = 0;
-            for (int i = 0; i < numberCycles + 1; i++) {
+            for (int i = 0; i < numberCycles; i++) {
 
                 //Cycle line 1 - cycle count
                 outFile.println("*** Cycle #: " + i + " ***");
@@ -98,7 +128,7 @@ public class SingleCoreScheduler
                     if (readyQ.peek().getLength() == (i - readyQ.peek().getStart())) {
                         int pid = readyQ.peek().getPid();
                         readyQ.remove();
-                        outFile.printf("Process #%d has just been terminated.%n", pid);
+                        outFile.printf("Process #%d has just terminated.%n", pid);
                         numberComp++;
                     } else {
                         outFile.printf("Process #%d is executing.%n", readyQ.peek().getPid());
@@ -122,20 +152,20 @@ public class SingleCoreScheduler
                     iCS = true;
                     PCB newPCB = new PCB(id, priority, 0, i, length);
                     readyQ.insert(newPCB);
-                    outFile.printf("Adding job with pid %d and priority %d and length %d%n",
+                    outFile.printf("Adding job with pid #%d and priority %d and length %d.%n",
                             id, priority, length);
                 } else {
                     outFile.println("No new job this cycle.");
                     iCS = false;
                 }
             }
-            double pPC = numberComp / (numberCycles + 1);
-            outFile.printf("The average throughput is %.5f per cycle.%n", pPC);
-
-            double avgWait = totalWait / numberComp;
-            outFile.printf("The average wait time is %.1f", avgWait);
             inFile.close();
         }
+        double pPC = numberComp / (numberCycles + 1);
+        outFile.printf("The average throughput is %.5f per cycle. %n", pPC);
+
+        double avgWait = totalWait / numberComp;
+        outFile.printf("The average wait time is %.1f.", avgWait);
     }
 }
 
